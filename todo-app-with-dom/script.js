9b0978e6b9a87eda169ca72d6bf2a1160f91f8b4
Chandan-Ojha <chandanojha776@gmail.com>
2022-08-28 20:15:38 +0600

    todo-app initial setup done

---
+++ todo-app-with-dom/script.js
@@ -0,0 +1,7 @@
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Chandan Ojha
+ * Date: 28/08/2022
+ *
+ */
