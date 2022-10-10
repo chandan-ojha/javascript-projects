ed91685eff75856f63a2475ea471a4b87afa5859
Chandan Ojha <chandanojha776@gmail.com>
2022-10-10 16:26:39 +0600

    Restaurant Menu Project Completed

--- Restaurant Menu Page/app.js
+++ Restaurant Menu Page/app.js
@@ -1,3 +1,7 @@
+//get only unique categories - HARDEST ONE
+//iterate over categories return buttons
+//make sure to select buttons when they are available
+
 //items
 const menu = [
     {
@@ -82,25 +86,81 @@ const menu = [
     },
 ];
 
-//get parent element
+// get parent element
 const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
 
-//display all items when page loads
-window.addEventListener('DOMContentLoaded',function (){
-    let displayMenu = menu.map(function (item) {
-        return `<article class="menu-item">
-       <img src=${item.img} class="photo" alt=${item.title}>
-       <div class="item-info">
-         <header>
-           <h4>${item.title}</h4>
-           <h4 class="price">$${item.price}</h4>
-         </header>
-         <p class="item-text">
-           ${item.desc}
-         </p>
-       </div>
-    </article>`;
-    });
-    displayMenu = displayMenu.join('');
-    sectionCenter.innerHTML = displayMenu
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+    displayMenuButtons();
 });
+
+//display menu items
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+        return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+
+//display menu buttons
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+        })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    //console.log(filterBtns);
+
+    /*filter items*/
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                diplayMenuItems(menu);
+            } else {
+                diplayMenuItems(menuCategory);
+            }
+        });
+    });
+}
+
+
+
+
